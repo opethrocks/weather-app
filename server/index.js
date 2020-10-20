f96cf73e405b949793483d9f75dec20c8d689b84
opethrocks <lebleu131@icloud.com>
2020-10-20 10:43:44 -0500

    Added autocomplete functionality

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const cityMatch = require('./routes/api/citySearch');
+const autoComplete = require('./routes/api/autoComplete');
+
 app.use('/api/citySearch', cityMatch);
+app.use('/api/autoComplete', autoComplete);
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(__dirname + '/public'));
   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
---
+++ server/routes/api/autoComplete.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('fs');
+
+const router = express.Router();
+
+const rawData = JSON.parse(fs.readFileSync('./cities.json', 'utf8'));
+
+router.get('/', (req, res) => {
+  const searchString = req.query.q;
+  const state = req.query.state;
+  const country = req.query.country;
+  const reqQuery = req.query;
+
+  if (state) {
+    res.send(
+      rawData.filter((obj) => {
+        return obj.state === state && obj.name.includes(searchString);
+      })
+    );
+  } else if (country) {
+    res.send(
+      rawData.filter((obj) => {
+        return obj.country === country && obj.name.includes(searchString);
+      })
+    );
+  }
+});
+
+module.exports = router;
